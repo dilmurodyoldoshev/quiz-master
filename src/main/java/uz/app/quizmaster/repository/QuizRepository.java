452635a5e2fcf572997602aa3265e0fc4527595f
dilmurodyoldoshev <yoldoshevdilmurod99@gmail.com>
2025-09-08 12:07:15 +0500

    fix: correct QuizRepository method parameter type from Object to User

--- src/main/java/uz/app/quizmaster/repository/QuizRepository.java
+++ src/main/java/uz/app/quizmaster/repository/QuizRepository.java
@@ -2,9 +2,10 @@ package uz.app.quizmaster.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.app.quizmaster.entity.Quiz;
+import uz.app.quizmaster.entity.User;
 
 import java.util.List;
 
 public interface QuizRepository extends JpaRepository<Quiz, Integer> {
-    List<Quiz> findByCreatedBy(Object teacher);
+    List<Quiz> findByCreatedBy(User teacher);
 }
