9fb065e35af21d22dda5bfd2e76e83a4ee18c658
dilmurodyoldoshev <yoldoshevdilmurod99@gmail.com>
2025-09-13 13:49:03 +0500

    GlobalEcceptionHandler was deleted

--- src/main/java/uz/app/quizmaster/entity/Quiz.java
+++ src/main/java/uz/app/quizmaster/entity/Quiz.java
@@ -1,5 +1,6 @@
 package uz.app.quizmaster.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "quizzes")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Quiz {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
