721a22727ce96ee2017e940237b3a87ff676bb92
dilmurodyoldoshev <yoldoshevdilmurod99@gmail.com>
2025-09-17 05:58:57 +0500

    refactor(service): update ResultService interface with new methods

--- src/main/java/uz/app/quizmaster/service/ResultService.java
+++ src/main/java/uz/app/quizmaster/service/ResultService.java
@@ -1,6 +1,7 @@
 package uz.app.quizmaster.service;
 
 import uz.app.quizmaster.dto.LeaderboardEntryDto;
+import uz.app.quizmaster.dto.ResultDto;
 import uz.app.quizmaster.entity.Result;
 import uz.app.quizmaster.payload.ResponseMessage;
 
@@ -9,4 +10,5 @@ import java.util.List;
 public interface ResultService {
     ResponseMessage<Result> calculateResult(Integer attemptId);
     ResponseMessage<List<LeaderboardEntryDto>> getLeaderboard(Integer quizId);
+    ResponseMessage<List<ResultDto>> getResultsForCurrentUser();
 }
