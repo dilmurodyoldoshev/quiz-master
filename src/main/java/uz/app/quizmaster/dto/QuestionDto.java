59c108e9671aec4e82f57ce3888eba2b324c67ee
dilmurodyoldoshev <yoldoshevdilmurod99@gmail.com>
2025-09-17 05:58:56 +0500

    refactor(dto): improve QuestionDto fields and structure

--- src/main/java/uz/app/quizmaster/dto/QuestionDto.java
+++ src/main/java/uz/app/quizmaster/dto/QuestionDto.java
@@ -5,6 +5,7 @@ import uz.app.quizmaster.enums.AnswerType;
 
 @Data
 public class QuestionDto {
+    private Integer id;
     private String text;
     private String optionA;
     private String optionB;
