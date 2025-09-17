d435c8f6a4f656366e4df75f26dbc0ce6a54c598
dilmurodyoldoshev <yoldoshevdilmurod99@gmail.com>
2025-09-17 05:58:56 +0500

    refactor(dto): enhance ResultDto for result response mapping

--- src/main/java/uz/app/quizmaster/dto/ResultDto.java
+++ src/main/java/uz/app/quizmaster/dto/ResultDto.java
@@ -1,4 +1,18 @@
 package uz.app.quizmaster.dto;
 
-public class d {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResultDto {
+    private Integer quizId;
+    private String quizTitle;
+    private Integer score;
+    private LocalDateTime completedAt;
+    private Integer rank; // optional, agar hisoblasangiz to'ldiriladi
 }
